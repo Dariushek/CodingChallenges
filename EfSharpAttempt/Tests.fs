d3f7dab067e4f110c60571b1961222320b749770
Dariushek <flash160@wp.pl>
2020-03-10 23:33:18 +0100

    Sample codes

--- EfSharpAttempt/Tests.fs
+++ EfSharpAttempt/Tests.fs
@@ -4,4 +4,14 @@ open Xunit
 
 [<Fact>]
 let ``Test`` () =
+    let sumOfSquares list =
+        list 
+        |> List.map (fun x -> x * x)
+        |> List.sum
+
+    let sum = sumOfSquares [1..10]
+    printf "%d" sum
+    printf "%A" [1..100]
+    
+
     Assert.True(true)
