7eb58287480f25b9314c1973b6a07010d86e5729
Dariuszek <dariuszborawskidb@gmail.com>
2022-06-21 18:03:00 +0200

    Palindrome validator

---
+++ SimpleAlgorithms/Palindrome/Palindrome.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleAlgorithms.Palindrome;
+
+public static class Palindrome
+{
+    public static bool Validate(string word)
+    {
+        int half = word.Length / 2;
+        int j = word.Length - 1;
+        for (var i = 0; i < half; i++, j--)
+        {
+            if (char.ToLower(word[i]) != char.ToLower(word[j]))
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ SimpleAlgorithms/Palindrome/PalindromeTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace SimpleAlgorithms.Palindrome;
+
+public class PalindromeTests
+{
+    [Theory]
+    [InlineData("level")]
+    [InlineData("kayak")]
+    [InlineData("noon")]
+    [InlineData("RaceCar")]
+    public void ValidatePalindromeValidatesSingleWordsCorrectly(string word)
+    {
+        Palindrome.Validate(word).Should().Be(true);
+    }
+}
