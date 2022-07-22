7935cf90c417c297798fe7695b23401d9d399951
Dariuszek <flash160@wp.pl>
2022-07-22 15:54:33 +0200

    Case without lock

--- Multithreading/Tests.cs
+++ Multithreading/Tests.cs
@@ -3,37 +3,58 @@ using System.Threading;
 using FluentAssertions;
 using Xunit;
 
-namespace Multithreading
+namespace Multithreading;
+
+public class Tests
 {
-    public class Tests
+    private readonly object incrementingLock = new();
+
+    [Fact]
+    public void LockedVariableIncrementedByTwoThreadsHasProperValue()
     {
-        private readonly object incrementingLock = new object();
-        [Fact]
-        public void LockedVariableIncrementedByTwoThreadsHasProperValue()
+        var index = 0;
+
+        ThreadStart increment = () =>
         {
-            int i = 0;
-
-            ThreadStart increment = () =>
+            foreach (int _ in Enumerable.Range(1, 10000))
             {
-                foreach (int _ in Enumerable.Range(1,10000))
-                {
-                    lock (incrementingLock)
-                    {
-                        i++;
-                    }
-                }
-            };
-            
-            var thread1 = new Thread(increment);
-            var thread2 = new Thread(increment);
-            
-            thread1.Start();
-            thread2.Start();
+                lock (incrementingLock)
+                    index++;
+            }
+        };
 
-            thread1.Join();
-            thread2.Join();
+        var thread1 = new Thread(increment);
+        var thread2 = new Thread(increment);
 
-            i.Should().Be(20000);
-        }
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        index.Should().Be(20000);
+    }
+
+    [Fact]
+    public void VariableIncrementedByTwoThreadsHasInvalidValue()
+    {
+        var index = 0;
+
+        ThreadStart increment = () =>
+        {
+            foreach (int _ in Enumerable.Range(1, 10000))
+                index++;
+        };
+
+        var thread1 = new Thread(increment);
+        var thread2 = new Thread(increment);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        index.Should().NotBe(20000);
     }
 }
