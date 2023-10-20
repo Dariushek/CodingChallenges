aaae0af1c2ab805637612d955bb3999ab2c6d23c
Dariuszek <dariuszborawskidb@gmail.com>
2023-10-20 14:42:51 +0200

    Finish string enum

---
+++ CleverSnippets/StringEnum/StringEnumBase.cs
@@ -0,0 +1,11 @@
+﻿namespace CleverSnippets;
+
+public abstract record StringEnumBase(string Value)
+{
+    public string Value { get; } = Value;
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
---
+++ CleverSnippets/StringEnum/StringEnumTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace CleverSnippets;
+
+public class StringEnumTests
+{
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public StringEnumTests(ITestOutputHelper testOutputHelper)
+    {
+        this.testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Test()
+    {
+        StringEnum optionA = StringEnum.OptionA;
+        StringEnum optionA2 = StringEnum.OptionA;
+        StringEnum optionB = StringEnum.OptionB;
+
+        testOutputHelper.WriteLine(optionA.ToString());
+        testOutputHelper.WriteLine(optionB.ToString());
+
+        optionA.Should().BeEquivalentTo(optionA2);
+        optionA.Should().Be(optionA2);
+
+        optionA.Should().NotBe(optionB);
+        optionA.Should().NotBeEquivalentTo(optionB);
+
+        (optionA == optionA2).Should().BeTrue();
+        (optionA != optionB).Should().BeTrue();
+    }
+}
